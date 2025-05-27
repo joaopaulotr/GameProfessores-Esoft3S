ca2e6ecca65b3de0d00de7784d88e1e6cbb2056f
duudafs <dudascomparin@gmail.com>
2025-05-27 15:19:32 -0300

    player andando direita

--- vue/src/components/PlayerSprite.vue
+++ vue/src/components/PlayerSprite.vue
@@ -48,12 +48,17 @@ const frames = {
     new URL('@/assets/images/walkplayer6.png', import.meta.url).href
   ],
   right: [
-    new URL('@/assets/images/walkplayer1.png', import.meta.url).href,
-    new URL('@/assets/images/walkplayer2.png', import.meta.url).href,
-    new URL('@/assets/images/walkplayer3.png', import.meta.url).href,
-    new URL('@/assets/images/walkplayer4.png', import.meta.url).href,
-    new URL('@/assets/images/walkplayer5.png', import.meta.url).href,
-    new URL('@/assets/images/walkplayer6.png', import.meta.url).href
+    new URL('@/assets/images/walkright1.png', import.meta.url).href,
+    new URL('@/assets/images/walkright2.png', import.meta.url).href,
+    new URL('@/assets/images/walkright3.png', import.meta.url).href,
+    new URL('@/assets/images/walkright4.png', import.meta.url).href,
+    new URL('@/assets/images/walkright5.png', import.meta.url).href,
+    new URL('@/assets/images/walkright6.png', import.meta.url).href,
+    new URL('@/assets/images/walkright7.png', import.meta.url).href,
+    new URL('@/assets/images/walkright8.png', import.meta.url).href,
+    new URL('@/assets/images/walkright9.png', import.meta.url).href,
+    new URL('@/assets/images/walkright10.png', import.meta.url).href,
+    new URL('@/assets/images/walkright11.png', import.meta.url).href
   ]
 }
 
